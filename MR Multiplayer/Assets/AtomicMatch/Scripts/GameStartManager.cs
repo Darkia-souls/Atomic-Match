d450e0468a98d9f21ef4b719a75c0bbc65065b09
Devybara <thiago.senadantas@hyperisland.se>
2025-03-23 13:40:55 +0100

    UI instructions WIP

---
+++ MR Multiplayer/Assets/AtomicMatch/Scripts/GameStartManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class GameStartManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;  // Reference to the VideoPlayer component
+    public Button playButton;        // Reference to the Play button
+    public GameObject[] objectsToActivate; // Objects to activate
+    public Canvas uiCanvas;          // Reference to the UI canvas
+
+    void Start()
+    {
+        // Deactivate objects at the start
+        DeactivateObjects();
+        
+        videoPlayer.Play();
+        
+        // Set up the Play button click event
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+        
+    }
+    void OnPlayButtonClicked()
+    {
+       // Activate the objects when the Play button is clicked
+        ActivateObjects();
+
+        // Destroy the UI canvas
+        Destroy(uiCanvas.gameObject);
+
+        // the video stops
+        videoPlayer.Stop();
+
+    }
+    void DeactivateObjects()
+    {
+        // Deactivate all objects in the array at the beginning
+        foreach (var obj in objectsToActivate)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    void ActivateObjects()
+    {
+        // Activate all objects when the button is clicked
+        foreach (var obj in objectsToActivate)
+        {
+            obj.SetActive(true);
+        }
+    }
+}
